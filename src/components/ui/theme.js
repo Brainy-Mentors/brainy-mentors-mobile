34f50e16cc3329b22143364a2d73e2ea8521ec51
Gary HL <garyhl321@gmail.com>
2023-08-10 20:11:52 +0200

    created theme

---
+++ src/components/ui/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+  colors: {
+    textPrimary: "#DBB38E",
+    white: "#E6E4DE",
+    primary: "#DBB38E",
+    black: "#000",
+    backgroundBase: "#2A2A2A",
+    backgrounBaseLith: "#B7B7B7",
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+    title: 20,
+    big: 24,
+    extraBig: 28,
+  },
+  fonts: {
+    main: "System",
+  },
+  fontWeights: {
+    normal: "400",
+    bold: "700",
+  },
+};
+
+export default theme;
