6d7077bb7dc2a9b70ee9e6473d84773db8bad1cc
Gary HL <garyhl321@gmail.com>
2023-08-15 01:01:43 +0200

    created dataMentors constant

---
+++ src/constants/dataMentors.js
@@ -0,0 +1,37 @@
+export const dataMentors = [
+  {
+    name: "Juan ProgramaYa",
+    specialization: "Experto en Desarrollo de Software",
+    shortDescription:
+      "Aprende a programar con Juan. Descubre los secretos del código y conviértete en un mago del desarrollo de software.",
+    imageUrl: "URL_de_la_imagen", // Reemplaza con la URL de la imagen del mentor programador
+  },
+  {
+    name: "David PotencialMax",
+    specialization: "Coach de Desarrollo Personal",
+    shortDescription:
+      "Desbloquea tu potencial con David. Supera obstáculos y logra tus objetivos personales con su orientación experta.",
+    imageUrl: "URL_de_la_imagen", // Reemplaza con la URL de la imagen del mentor de desarrollo personal
+  },
+  {
+    name: "Alejandro FitVida",
+    specialization: "Entrenador de Fitness",
+    shortDescription:
+      "Transforma tu vida con Alejandro. Descubre cómo alcanzar un estilo de vida saludable y activo a través del fitness.",
+    imageUrl: "URL_de_la_imagen", // Reemplaza con la URL de la imagen del mentor de fitness
+  },
+  {
+    name: "Roberto MenteClara",
+    specialization: "Experto en Psicología",
+    shortDescription:
+      "Encuentra claridad mental con Roberto. Supera desafíos y cultiva una mentalidad positiva y resiliente con su guía.",
+    imageUrl: "URL_de_la_imagen", // Reemplaza con la URL de la imagen del mentor de psicología
+  },
+  {
+    name: "Fernando RelacionesTrue",
+    specialization: "Experto en Relaciones Auténticas",
+    shortDescription:
+      "Domina el arte de las relaciones con Fernando. Aprende a construir conexiones genuinas y a comunicarte con impacto.",
+    imageUrl: "URL_de_la_imagen", // Reemplaza con la URL de la imagen del mentor de relaciones y seducción
+  },
+];
