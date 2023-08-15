a633470fc7ba30685b7fa9d533cd0d05eeae2ed0
Gary HL <garyhl321@gmail.com>
2023-08-15 02:35:21 +0200

    created new screen route Selected mentor

--- src/navigation/MainStack.jsx
+++ src/navigation/MainStack.jsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import Home from "../screens/home/Home";
 import Profile from "../screens/profile/Profile";
+import SelectMentor from "../screens/selectMentor/SelectMentor";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,8 +15,9 @@ export default function MainStack() {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="select-mentor" component={SelectMentor} />
         <Stack.Screen name="profile" component={Profile} />
+        <Stack.Screen name="home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
