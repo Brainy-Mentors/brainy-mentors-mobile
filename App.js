3a8b2b6487d21d934052d75ec5f9a2896e3fd818
Gary HL <garyhl321@gmail.com>
2023-08-29 22:23:54 +0200

    install i18 next and config es/en traductions

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import MainStack from "./src/navigation/MainStack";
 import { AppProvider } from "./src/context/AppContext";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import en from "./src/translations/en.json";
+import es from "./src/translations/es.json";
+
+export const languageSrc = {
+  en: { global: en },
+  es: { global: es },
+};
+
+i18next.init({
+  compatibilityJSON: "v3",
+  lng: "en",
+  fallbackLng: "en",
+  resources: languageSrc,
+});
 
 export default function App() {
   return (
-    <AppProvider>
-      <MainStack />
-    </AppProvider>
+    <I18nextProvider i18n={i18next}>
+      <AppProvider>
+        <MainStack />
+      </AppProvider>
+    </I18nextProvider>
   );
 }
