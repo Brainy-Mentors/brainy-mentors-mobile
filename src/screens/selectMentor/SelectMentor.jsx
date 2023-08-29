a89b1f357e4bec53849d4f46ff70443dabf04771
Gary HL <garyhl321@gmail.com>
2023-08-29 22:25:38 +0200

    cleaning components

--- src/screens/selectMentor/SelectMentor.jsx
+++ src/screens/selectMentor/SelectMentor.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ScreenBase from "../ScreenBase";
 import MentorsList from "../../components/mentorList/MentorList";
 
-export default function SelectMentor({ navigation }) {
+export default function SelectMentor() {
   return (
     <ScreenBase complete>
       <MentorsList />
